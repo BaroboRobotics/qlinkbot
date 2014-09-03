36efab555e06587b1a2d94732c9f2e43eb611dd9
Harris Hancock <vortrab@gmail.com>
2014-09-03 15:17:38 -0700

    Implement more stubs

--- src/qlinkbot.cpp
+++ src/qlinkbot.cpp
@@ -14,11 +14,24 @@
 
 #include <iostream>
 
+#undef M_PI
+#define M_PI 3.14159265358979323846
+
+namespace {
+
+template <class T>
+T degToRad (T x) { return T(double(x) * M_PI / 180.0); }
+
+template <class T>
+T radToDeg (T x) { return T(double(x) * 180.0 / M_PI); }
+
 inline QDebug operator<< (QDebug dbg, const rpc::VersionTriplet& triplet) {
     QDebugStateSaver s { dbg };
     dbg.nospace() << triplet.major() << '.' << triplet.minor() << '.' << triplet.patch();
     return dbg;
 }
+
+} // file namespace
 
 using MethodIn = rpc::MethodIn<barobo::Robot>;
 
@@ -158,50 +171,85 @@ void QLinkbot::newMotorValues(double j1, double j2, double j3, int mask)
     }
 }
 
-
-int QLinkbot::setJointSpeeds (double, double, double) {
-#warning Unimplemented stub function in qlinkbot
-    qWarning() << "Unimplemented stub function in qlinkbot";
-    return 0;
-}
-int QLinkbot::disableAccelEventCallback () {
-#warning Unimplemented stub function in qlinkbot
-    qWarning() << "Unimplemented stub function in qlinkbot";
-    return 0;
-}
-int QLinkbot::disableButtonCallback () {
-#warning Unimplemented stub function in qlinkbot
-    qWarning() << "Unimplemented stub function in qlinkbot";
-    return 0;
-}
-int QLinkbot::disableJointEventCallback () {
-#warning Unimplemented stub function in qlinkbot
-    qWarning() << "Unimplemented stub function in qlinkbot";
-    return 0;
-}
-int QLinkbot::getJointAngles (double& e1, double& e2, double& e3, int) {
+int QLinkbot::setJointSpeeds (double s0, double s1, double s2) {
     try {
-        auto values = m->proxy.fire(MethodIn::getEncoderValues{}).get();
-        assert(values.values_count >= 3);
-        e1 = values.values[0];
-        e2 = values.values[1];
-        e3 = values.values[2];
+        auto f0 = m->proxy.fire(MethodIn::setMotorControllerOmega { 0, float(degToRad(s0)) });
+        auto f1 = m->proxy.fire(MethodIn::setMotorControllerOmega { 1, float(degToRad(s1)) });
+        auto f2 = m->proxy.fire(MethodIn::setMotorControllerOmega { 2, float(degToRad(s2)) });
+        f0.get();
+        f1.get();
+        f2.get();
     }
     catch (...) {
         return -1;
     }
     return 0;
 }
-int QLinkbot::moveNB (double, double, double) {
+
+int QLinkbot::disableAccelEventCallback () {
 #warning Unimplemented stub function in qlinkbot
     qWarning() << "Unimplemented stub function in qlinkbot";
     return 0;
 }
-int QLinkbot::moveToNB (double, double, double) {
+
+int QLinkbot::disableButtonCallback () {
+    try {
+        m->proxy.fire(MethodIn::enableButtonEvent{false}).get();
+    }
+    catch (...) {
+        return -1;
+    }
+    return 0;
+}
+
+int QLinkbot::disableJointEventCallback () {
 #warning Unimplemented stub function in qlinkbot
     qWarning() << "Unimplemented stub function in qlinkbot";
     return 0;
 }
+
+int QLinkbot::getJointAngles (double& a0, double& a1, double& a2, int) {
+    try {
+        auto values = m->proxy.fire(MethodIn::getEncoderValues{}).get();
+        assert(values.values_count >= 3);
+        a0 = radToDeg(values.values[0]);
+        a1 = radToDeg(values.values[1]);
+        a2 = radToDeg(values.values[2]);
+    }
+    catch (...) {
+        return -1;
+    }
+    return 0;
+}
+
+int QLinkbot::moveNB (double a0, double a1, double a2) {
+    try {
+        m->proxy.fire(MethodIn::move {
+            true, { barobo_Robot_Goal_Type_RELATIVE, float(degToRad(a0)) },
+            true, { barobo_Robot_Goal_Type_RELATIVE, float(degToRad(a1)) },
+            true, { barobo_Robot_Goal_Type_RELATIVE, float(degToRad(a2)) }
+        }).get();
+    }
+    catch (...) {
+        return -1;
+    }
+    return 0;
+}
+
+int QLinkbot::moveToNB (double a0, double a1, double a2) {
+    try {
+        m->proxy.fire(MethodIn::move {
+            true, { barobo_Robot_Goal_Type_ABSOLUTE, float(degToRad(a0)) },
+            true, { barobo_Robot_Goal_Type_ABSOLUTE, float(degToRad(a1)) },
+            true, { barobo_Robot_Goal_Type_ABSOLUTE, float(degToRad(a2)) }
+        }).get();
+    }
+    catch (...) {
+        return -1;
+    }
+    return 0;
+}
+
 int QLinkbot::setColorRGB (int r, int g, int b) {
     try {
         m->proxy.fire(MethodIn::setLedColor{
@@ -213,6 +261,7 @@ int QLinkbot::setColorRGB (int r, int g, int b) {
     }
     return 0;
 }
+
 int QLinkbot::setJointEventThreshold (int, double) {
 #warning Unimplemented stub function in qlinkbot
     qWarning() << "Unimplemented stub function in qlinkbot";
